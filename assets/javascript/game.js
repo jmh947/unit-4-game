3d520ae6e926e03ea9f1f5398be92cc0cc74f053
Jaclyn Hardy <jackihardy@yahoo.com>
2020-02-23 14:16:16 -0800

    added images and some css with initia javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+//Enter variables here!
+//Variable for diamond 1 between 1-12
+ var num1= Math.floor(Math.random() *11+1)
+//Variable for diamond 2 between 1-12
+var num2= Math.floor(Math.random() *11+1)
+//Variable for diamond 3 between 1-12
+var num3= Math.floor(Math.random() *11+1)
+//Variable for diamond 4 between 1-12
+var num4= Math.floor(Math.random() *11+1)
